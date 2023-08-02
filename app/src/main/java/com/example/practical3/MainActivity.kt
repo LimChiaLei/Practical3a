4ddb6d5cfa0ce5bb0b8b80b751e34a81ab9b98c3
anreh <anurehka@gmail.com>
2023-08-02 10:32:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/practical3/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.practical3
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setListener()
+    }
+
+    private fun makeColored(view:View){
+        when(view.id){
+            R.id.box_one_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GREEN)
+            R.id.box_three_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_four_text -> view.setBackgroundColor(Color.RED)
+            R.id.box_five_text -> view.setBackgroundColor(Color.CYAN)
+            else -> view.setBackgroundColor(Color.DKGRAY)
+            //R.id.red_button -> findViewById<TextView>(R.id.box_three_text)
+        }
+    }
+
+    private fun setListener(){
+        val boxOneText = findViewById<TextView>(R.id.box_one_text)
+        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
+        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
+        val boxFourText = findViewById<TextView>(R.id.box_four_text)
+        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+
+        val clickableList:List<View> = listOf(boxOneText,boxTwoText,boxThreeText,boxFourText,boxFiveText,rootConstraintLayout)
+
+        //boxOneText.setOnClickListener { makeColored(it) }
+        //boxTwoText.setOnClickListener { makeColored(it) }
+        //boxThreeText.setOnClickListener { makeColored(it) }
+        //boxFourText.setOnClickListener { makeColored(it) }
+        //boxFiveText.setOnClickListener { makeColored(it) }
+
+        for(item in clickableList){
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+
+
+}
